4ab3ab19c6b8bc699bae212b50dd7a8cc3874009
afan104 <amyfan061@gmail.com>
2025-02-01 13:50:37 -0700

    debugging spiArray

--- jsonToImgs.js
+++ jsonToImgs.js
@@ -4,23 +4,22 @@
 
 // initialize doc reference
 var doc = app.activeDocument
+
 // read in json and save info into dictionary
 var dataPath = "~/Desktop/capaldi/jsonFiles/"
-var jsonFilePath = dataPath + doc.name.slice(0, -4) + ".json"
+var jsonFilePath = dataPath + doc.name.slice(0, -4) + ".json" // find by replacing .png with .json
 var jsonShapeData = readJsonFile(jsonFilePath)["shapes"]
 
 // grab all the paths from json
-var pathItemsDict = {"normal": [], "kogg": []}  // value is a list of shapes; shape is a list of points; 3D tensor-like shape
+var shapesDict = {"normal": [], "kogg": []}  // value is a list of shapes; shape is a list of points; 3D tensor-like shape
 for (var i = 0; i < jsonShapeData.length; i++) {
   var shapeData = jsonShapeData[i]
   var layer = shapeData["label"]
   var shape = shapeData["points"]
 
-  pathItemsDict[layer].push(shape)
+  shapesDict[layer].push(shape)
 }
 
-// TODO: initialize layers with vector masks
-alert("1")
-createVectorLayer("normal")
-createVectorLayer("kogg")
-alert("3")
+// Add paths to each layer
+addPathItem(doc, "kogg", shapesDict)
+// addPathItem(doc, "normal", shapesDict)
--- utils.js
+++ utils.js
@@ -22,22 +22,88 @@ function readJsonFile(jsonFilePath) {
   }
 }
 
-// create new layer function
-createVectorLayer = function (doc, shapesDict, name) {
-  // create new layer
-  var layerRef = doc.artLayers.add()
-  layerRef.name = name
+// function to create pathPointInfo object
+function getPathPointInfo(pointPosition) {
+  var ppi = new PathPointInfo()
+  ppi.kind = PointKind.CORNERPOINT
+  ppi.anchor = pointPosition
+  ppi.leftDirection = pointPosition
+  ppi.RightDirection = pointPosition
+  return ppi
+}
 
-  // set as active layer
-  doc.activeLayer = layerRef
+// function to create subpathInfo object
+function getSubPathInfo(shape) {
+  var entireSubPath = []
 
-  // create path item containing all active layer shapes and change it to a vector mask
-  var subpathItems = shapesDict[name]
-  var pathItem = doc.pathItems.add(subpathItems)
-  pathItem.kind = PathKind.VECTORMASK
+  for (i = 0; i < shape.length; i++) {
+    var point = shape[i]
+    var ppi = getPathPointInfo(point)
 
-  alert("2")
-  // link the path item to the layer
-  layerRef.pathItems = pathItem
-  alert("3")
+    // add to subpath
+    entireSubPath.push(ppi)
+    alert("adding point to subpath")
+  }
+
+  // define SubPathInfo as an object
+  var spi = new SubPathInfo()
+  spi.entireSubPath = entireSubPath
+  spi.closed = true
+  return spi
+}
+
+// function to create and add PathItem
+function addPathItem(doc, name, shapesDict) {
+  // create layer for vector mask and set active layer
+  var layer = doc.artLayers.add()
+  layer.name = name
+  doc.activeLayer = layer
+
+  // get all shapes (array of subpathinfo objects)
+  var spiArray = []
+  var shapes = shapesDict[name]
+  shapes = [
+    [
+      [1, 2],
+      [3, 5],
+      [, 4, 6],
+    ],
+    [
+      [7, 8],
+      [9, 10],
+      [11, 12],
+    ],
+  ] //debug
+
+  for (var i = 0; i < shapes.length; i++) {
+    var shape = shapes[i]
+    var spi = getSubPathInfo(shape)
+    spiArray.push(spi)
+    alert("added subpath info to spiArray")
+    if (spiArray.length == 2) {
+      break // get two shapes
+    }
+  }
+
+  alert("checking spiArray contains subpath info")
+  for (var i = 0; i < spiArray.length; i++) {
+    for (var j = 0; j < spiArray[i].entireSubPath.length; j++) {
+      alert("anchor")
+      alert(spiArray[i].entireSubPath[j].anchor)
+      alert(typeof spiArray[i].entireSubPath[j].anchor[1])
+      alert("left")
+      alert(spiArray[i].entireSubPath[j].leftDirection)
+      alert("right")
+      alert(spiArray[i].entireSubPath[j].rightDirection)
+      alert("kind")
+      alert(spiArray[i].entireSubPath[j].kind)
+    }
+  }
+  // Check the contents of spiArray
+  var pathItem = doc.pathItems.add(name, spiArray)
+  alert(2)
+  pathItem.PathKind = PathKind.VECTORMASK
+
+  // add stroke to visualize
+  pathItem.strokePath(ToolType.PENCIL)
 }
